2b31bd9788c60d5cc1d6395e78592efe5d57edd4
Sahan <mohammedshn2002@gmail.com>
2026-03-08 11:53:59 +0530

    Define API routes for auth and task CRUD

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Public routes
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+// Protected routes
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/user', [AuthController::class, 'user']);
+
+    Route::apiResource('tasks', TaskController::class);
+    Route::get('/tasks-trashed', [TaskController::class, 'trashed']);
+    Route::patch('/tasks/{id}/restore', [TaskController::class, 'restore']);
+    Route::delete('/tasks/{id}/force-delete', [TaskController::class, 'forceDelete']);
+});
